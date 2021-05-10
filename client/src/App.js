c8381059d559a5e95ae1efadddbec5c201ec000e
phamthomas12 <phamthomas12@gmail.com>
2021-05-11 01:05:46 +1000

    start from scratch

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,29 @@
 import React from "react";
-import NavBar from "./components/NavBar";
-import Dashboard from "./pages/dashboard";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
 
 function App() {
   return (
-    <div>
-      <NavBar />
-      <Dashboard />
-    </div>
-  )
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path={["/", "/books"]}>
+            <Books />
+          </Route>
+          <Route exact path="/books/:id">
+            <Detail />
+          </Route>
+          <Route>
+            <NoMatch />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
