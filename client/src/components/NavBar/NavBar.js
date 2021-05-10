76b38b5fb5b96b63c8a25a53e200ab4003b0f500
phamthomas12 <phamthomas12@gmail.com>
2021-05-11 00:43:06 +1000

    added a NavBar

---
+++ client/src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Navbar.css";
+
+function NavBar() {
+    return(
+<nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <a className="navbar-brand" href="/">
+        Castle On The Hill
+      </a>
+    </nav>
+
+    );
+}
+
+export default NavBar;
