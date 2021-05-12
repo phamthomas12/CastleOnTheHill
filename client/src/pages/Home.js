88b8702104fb29f043f268280137084f7a127168
phamthomas12 <phamthomas12@gmail.com>
2021-05-13 00:07:58 +1000

    removed unwanted component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,7 +2,6 @@ import React from "react";
 import HeaderImage from "../components/HeaderImage";
 import Layout from "../components/Layout";
 import NavBar from "../components/NavBar";
-import PostsList from "../components/PostList";
 
 function Home() {
   return (
@@ -10,7 +9,6 @@ function Home() {
     <HeaderImage />
     <Layout>
     <NavBar />
-    <PostsList />
     </Layout>
     </>
   );
